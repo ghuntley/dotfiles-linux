a3e6969fe247df54b4571910ec0743e5563ff350
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-09-07 02:54:19 +0000

    HTTP 302: Moved Permanently

--- plasma/.config/plasma-workspace/env/wm.sh
+++
@@ -1,7 +0,0 @@
-# Disable KWin and use i3gaps as WM
-export KDEWM=/run/current-system/sw/bin/i3
-
-
-# Compositor (Animations, Shadows, Transparency)
-# xcompmgr -C
-compton -b --config ~/.config/compton/compton.conf
--- scripts/toggle-compositor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pid=$(pidof compton)
-if [ $pid ]; then
-  echo "Disabling compositor"
-  killall compton
-else
-  echo "Enabling compositor"
-  compton -cCFb --backend glx --vsync opengl
-fi
